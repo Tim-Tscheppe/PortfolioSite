f6ddf8a18c66404ef306c4b9fff1e6ad58c88b42
Tim-Tscheppe <timothy.tscheppe@marquette.edu>
2023-06-13 19:01:13 -0500

    Fixed spacing in about section

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -25,7 +25,7 @@ export default function About() {
                 </a>
                 <a
                   href="#services"
-                  className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                  className="ml-4 inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
                   Services I Offer
                 </a>
               </div>
